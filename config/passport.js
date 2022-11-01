b8d560c704ee3a2e51e6b37b5d904d3f3f43bbed
SimonHung8 <simon850226@gmail.com>
2022-11-01 21:00:41 +0800

    feat: add google login feature

--- config/passport.js
+++ config/passport.js
@@ -1,5 +1,6 @@
 const passport = require('passport')
 const LocalStrategy = require('passport-local').Strategy
+const GoogleStrategy = require('passport-google-oauth20').Strategy
 const bcrypt = require('bcryptjs')
 const User = require('../models/User')
 
@@ -9,6 +10,7 @@ module.exports = app => {
   app.use(passport.session())
 
   // strategy
+  // local strategy
   passport.use(new LocalStrategy(
     { usernameField: 'email', passReqToCallback: true },
     (req, email, password, done) => {
@@ -24,6 +26,25 @@ module.exports = app => {
         .catch(err => done(err))
     }
   ))
+  // google strategy
+  passport.use(new GoogleStrategy({
+    clientID: process.env.GOOGLE_CLIENT_ID,
+    clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    callbackURL: process.env.GOOGLE_CLIENT_CALLBACK
+  }, (accessToken, refreshToken, profile, done) => {
+    const { email, name } = profile._json
+    User.findOne({ email })
+      .then(user => {
+        if (user) return done(null, user)
+        const randomPassword = Math.random().toString(36).slice(-8)
+        bcrypt
+          .genSalt(5)
+          .then(salt => bcrypt.hash(randomPassword, salt))
+          .then(hash => User.create({ name, email, password: hash }))
+          .then(user => done(null, user))
+          .catch(err => done(err))
+      })
+  }))
 
   // session
   passport.serializeUser((user, done) => {
